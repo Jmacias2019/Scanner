56178312db134212de1a1f7f68cab28878c156a3
Jacqueline Macias <jacqueline.gmacias@gmail.com>
2016-09-06 22:44:16 -0500

    Create Scan.java

---
+++ Scan.java
@@ -0,0 +1,30 @@
+package edu.saintjoe.cs.Jmacias2019.Scanner;
+
+public class Scan {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+/ ** A person has an age, weight and can report their info
+	* 
+	* @ author Jacqueline Macias
+	*
+	*/ 
+	public class Person{ 
+		private int age; //age in weight 
+		private double weight; // weight in pounds
+	/** Constructs a new instance of person with a particular 
+	 * age and weight
+	 */@ param an age of this person 
+	 public class( int a, double w){
+		 age = a;
+		 weight = w;
+	 }// Person(a,w)
+	 /** Returns a string containing instance properties*/
+	 public String toString(){
+		 return("My age is:" "+age" + "years" + "/n Myweight is +weight + "pounds");"
+		
+	 } //toString()
+}
+	}
+
+} //end of person class
